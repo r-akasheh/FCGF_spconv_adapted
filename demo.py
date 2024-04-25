932487259e09a9a3d6a892da685840d716f088c0
Rasseel Akasheh <rasseel.akasheh@outlook.com>
2024-04-25 10:10:20 +0200

    NOTASK new example for demo

--- demo.py
+++ demo.py
@@ -90,13 +90,13 @@ if __name__ == '__main__':
     # voxel_size = 0.1
     n_sample = 20000
 
-    #src_ply = o3d.io.read_point_cloud(src_path)
-    #tgt_ply = o3d.io.read_point_cloud(tgt_path)
-    #src_pcd = np.asarray(src_ply.points).astype(np.float32)
-    #tgt_pcd = np.asarray(tgt_ply.points).astype(np.float32)
+    # src_ply = o3d.io.read_point_cloud(src_path)
+    # tgt_ply = o3d.io.read_point_cloud(tgt_path)
+    # src_pcd = np.asarray(src_ply.points).astype(np.float32)
+    # tgt_pcd = np.asarray(tgt_ply.points).astype(np.float32)
 
-    path_to_npz_src = "C:/master/robot-vision-modul/category-level-alignment-3d-geofeatures/mesh_transform/npz_files/shoe-white_black_buckles002.npz"
-    path_to_npz_trg = "C:/master/robot-vision-modul/category-level-alignment-3d-geofeatures/mesh_transform/npz_files/shoe-hummel_green_sandal_right001.npz"
+    path_to_npz_src = "C:/master/robot-vision-modul/category-level-alignment-3d-geofeatures/mesh_transform/npz_files/teapot-wooden_color003.npz"
+    path_to_npz_trg = "C:/master/robot-vision-modul/category-level-alignment-3d-geofeatures/mesh_transform/npz_files/teapot-wooden_color.npz"
 
     # Load the point cloud data from the npz file
     data_src = np.load(path_to_npz_src)
